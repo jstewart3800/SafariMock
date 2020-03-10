1498713a31f681d8731a4c93686d8434b9bbc968
jstewart3800 <56611713+jstewart3800@users.noreply.github.com>
2020-03-10 14:42:15 -0700

    Gyro setup, halfway there

--- SafariMockPractice/src/app/home/home.page.ts
+++ SafariMockPractice/src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Gyroscope, GyroscopeOrientation, GyroscopeOptions } from '@ionic-native/gyroscope/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+      selector: 'app-home',
+      templateUrl: 'home.page.html',
+      styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+      public options: GyroscopeOptions = {
+            frequency: 1000
+      }
 
+      constructor(private gyroscope: Gyroscope) { }
+
+      getGyro() {
+            this.gyroscope.getCurrent(this.options)
+            .then((orientation: GyroscopeOrientation) => {
+                  alert('From Current: X: ' + orientation.x + 'Y: ' + orientation.y + 'Z: ' + orientation.z);
+                  console.log(orientation.x, orientation.y, orientation.z, orientation.timestamp);
+            })
+
+            .catch()
+      }
+
+      watchGyro() {
+            this.gyroscope.watch()
+            .subscribe((orientation: GyroscopeOrientation) => {
+                  alert('From Watch: X: ' + orientation.x + 'Y: ' + orientation.y + 'Z: ' + orientation.z);
+                  console.log(orientation.x, orientation.y, orientation.z, orientation.timestamp);
+            });     
+      }
 }
